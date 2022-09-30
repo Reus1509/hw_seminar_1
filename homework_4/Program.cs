4c722601a68d6a278d615ec8b8a5ca7b3355dbe7
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-10-01 02:58:36 +0300

    add homework_4

---
+++ homework_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 1;
+
+while (count <= number)
+{
+    if (count % 2 == 0) Console.Write(count + " ");
+    count++;
+}
